f776374585e9a2f72cf7d79140c721adb1440c23
Артём <mangustinio.007@gmail.com>
2021-10-21 20:54:04 +0200

    Сделал генерацию данных

--- js/main.js
+++ js/main.js
@@ -1,54 +1,84 @@
-
-const getRandomIntInclusive = function(min1, max1) {
-  let f = 0;
-  // eslint-disable-next-line id-length
-  let h = 0;
-  let someInt = 0;
-  if (min1 > max1) {
-    f = min1;
-    h = max1;
-    max1 = f;
-    min1 = h;
-  }
-  if (min1 <= 0, max1 <= 0) {
-    return 0;
-  } else if (min1 <= 0, max1 > 0) {
-    someInt = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
-    return someInt;
-  } else {
-    someInt = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
-    return someInt;
-  } //Максимум и минимум включаются
+const getRandomPositiveInteger = function(min1, max1) {
+  const lower = Math.ceil(Math.min(Math.abs(min1), Math.abs(max1)));
+  const upper = Math.floor(Math.max(Math.abs(min1), Math.abs(max1)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-const getRandomInclusive = function(degree,min1,max1) {
-  let f = 0;
-  let h = 0;
-  let someInt = 0;
-  if (min1 > max1) {
-    f = min1;
-    h = max1;
-    max1 = f;
-    min1 = h;
-  }
-  if (min1 <= 0, max1 <= 0) {
-    return 0;
-  } else if (min1 <= 0, max1 > 0) {
-    someInt = (Math.random() * (max1 - min1)) + min1;
-    someInt = someInt.toFixed(degree);
-    return someInt;
-  } else {
-    someInt = (Math.random() * (max1 - min1)) + min1;
-    someInt = someInt.toFixed(degree);
-    return someInt;
-  }
-};
-console.log(getRandomIntInclusive(15, 6));
-let k = 0;
-let result = 0;
-while (k<100) {
-  result = getRandomInclusive(2,1.1, 1.2);
-  k++;
-  console.log(result);
-}
 
-console.log((Math.random()*(1.2 - 1.1) + 1.1).toFixed(2));
+const getRandomPositiveFloat = function(min, max, digits = 1) {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+};
+
+const numberAvatar = getRandomPositiveInteger(1,10);
+const numberAvatarFix = `0${ numberAvatar}`;
+
+const typeOfRooms = ['palace','flat','house','bungalow','hotel'];
+const roomNumber = typeOfRooms.length - 1;
+const roomsChoice = typeOfRooms[roomNumber];
+
+const checkinVariants = ['12:00','13:00','14:00'];
+const checkinNumber = checkinVariants.length - 1;
+const checkinChoice = checkinVariants[checkinNumber];
+
+const checkoutVariants = ['12:00','13:00','14:00'];
+const checkoutNumber = checkoutVariants.length - 1;
+const checkoutChoice = checkoutVariants[checkinNumber];
+
+const typeOfFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const featureAmount = getRandomPositiveInteger(0,typeOfFeatures - 1);
+let featureChoice = 0;
+
+for(let i = 0; i <= typeOfFeatures - 1; i++){
+  let coin = getRandomPositiveInteger(1,2);
+  if (coin == 1){
+    featureChoice[i] = typeOfFeatures[i];
+  };
+};
+
+let allFeautures = '';
+for(let j = 1; j <= typeOfFeatures; j++){
+  allFeautures = allFeautures + ',' +  featureChoice[j];
+};
+
+
+const variationOfPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const photosNumber = variationOfPhotos.length - 1;
+const photosChoice = variationOfPhotos[photosNumber];
+
+let author = {
+  avatar: numberAvatarFix,
+};
+
+let offer = {
+  title: 'Введите текст',
+  addres: {
+    location: {
+      lat: getRandomPositiveFloat(35.65000,35.70000,5),
+      lng: getRandomPositiveFloat(139.70000,139.80000,5),
+    },
+  },
+  price: getRandomPositiveInteger(1,1000000),
+  rooms: roomChoice,
+  guests: getRandomPositiveInteger(1,100),
+  checkin: checkinChoice,
+  checkout: checkoutChoice,
+  features: featureChoice,
+  description: 'Описание',
+};
+
+
+
+
+
+
+
+
+
+
+console.log(numberAvatarFix);
+console.log(getRandomPositiveFloat(1.11,1.33,2));
