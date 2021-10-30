305e307e03bcca6a977e541f4dc7eea9b70801d0
Артём <mangustinio.007@gmail.com>
2021-10-30 11:56:03 +0200

    Сделал точку входа

--- js/utils/get-random-positive-integer.js
+++ js/utils/get-random-positive-integer.js
@@ -1,7 +1,7 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-let getRandomPositiveInteger = function(a, b) {
+const getRandomPositiveInteger = function(a, b) {
   // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
   // реализуем поддержку передачи минимального и максимального значения в любом порядке,
   // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
@@ -27,4 +27,4 @@ let getRandomPositiveInteger = function(a, b) {
   return Math.floor(result);
 };
 
-console.log(getRandomPositiveInteger(-7,1));
+export {getRandomPositiveInteger};
