1c7f817c19a6e85965123fd631ef59d0da7d2648
Артём <mangustinio.007@gmail.com>
2021-11-08 10:10:21 +0100

    Исправил ошибку с классами

--- js/form-data.js
+++ js/form-data.js
@@ -1,14 +1,28 @@
 const formType = document.querySelector('.ad-form');
 const mapFilterType = document.querySelector('.map__filters');
+const mapFilterChildrens = mapFilterType.children;
+const formChildrens = formType.children;
 
 const formDisactivation = function () {
-  formType.classList.add('.ad-form--disabled');
-  mapFilterType.classList.add('.map__filters--disabled');
+  formType.classList.add('ad-form--disabled');
+  mapFilterType.classList.add('map__filters--disabled');
+  for (const value of mapFilterChildrens) {
+    value.setAttribute('disabled','disabled');
+  }
+  for (const value of formChildrens) {
+    value.setAttribute('disabled','disabled');
+  }
 };
 
 const formActivation = function () {
-  formType.classList.remove('.ad-form--disabled');
-  mapFilterType.classList.remove('.map__filters--disabled');
+  formType.classList.remove('ad-form--disabled');
+  mapFilterType.classList.remove('map__filters--disabled');
+  for (const value of mapFilterChildrens) {
+    value.removeAttribute('disabled','disabled');
+  }
+  for (const value of formChildrens) {
+    value.removeAttribute('disabled','disabled');
+  }
 };
 
 export {formActivation, formDisactivation};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import {generateAds} from './utils/mock-data.js';
 import { popupFunction } from './card-data.js';
 import {formActivation,formDisactivation} from './form-data.js';
-formActivation();
+formDisactivation();
