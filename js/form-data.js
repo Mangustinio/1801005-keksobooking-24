cddf34cbef1b0fe75af4d4be854790bdcd674251
Артём <mangustinio.007@gmail.com>
2021-11-13 14:28:38 +0100

    функция с зависимостью комнат от гостей

--- js/form-data.js
+++ js/form-data.js
@@ -2,6 +2,49 @@ const formType = document.querySelector('.ad-form');
 const mapFilterType = document.querySelector('.map__filters');
 const mapFilterChildrens = mapFilterType.children;
 const formChildrens = formType.children;
+const selectPriceType = document.querySelector('#type');
+const inputPriceType = document.querySelector('#price');
+const selectRoomNumber = document.querySelector('#room_number');
+const roomNumberOption = selectRoomNumber.querySelectorAll('option');
+const selectCapacity = document.querySelector('#capacity');
+const TypeMinPriceMap = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+/*const roomNumberMap ={
+  1: 0,
+  2: 1,
+  3: 2,
+  100: 3,
+};
+*/
+const roomNumberOptionDisabledMap ={
+  1: roomNumberOption[1,2,3],
+  2: roomNumberOption[2,3],
+  3: roomNumberOption[3],
+  100: roomNumberOption[0,1,2],
+};
+
+const capacityRegulation = (event) => {
+  //const newCapacity = roomNumberMap[event.target.value];
+  const newOldCapacity = roomNumberOptionDisabledMap[event.target.value];
+  newOldCapacity.forEach((element) =>{
+    element.setAttribute('disabled','disabled');
+  });
+};
+
+selectRoomNumber.addEventListener('change', capacityRegulation);
+
+const changeMinPrice = (event) => {
+  const newPrice = TypeMinPriceMap[event.target.value];
+  inputPriceType.min = newPrice;
+};
+
+selectPriceType.addEventListener('change', changeMinPrice);
 
 const formDisactivation = function () {
   formType.classList.add('ad-form--disabled');
@@ -26,8 +69,3 @@ const formActivation = function () {
 };
 
 export {formActivation, formDisactivation};
-
-function onFilterChange (evt) {
-  let comparison = '';
-  comparison = evt.target.value;
-}
